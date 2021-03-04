0cf3d3ff1ece0202d578e0705ad745905608bb4b
felhipe1 <felhipe1@gmail.com>
2021-03-03 21:36:35 -0500

    Live_server_Live_sherad _11_02_2021

---
+++ app.php
@@ -0,0 +1,15 @@
+
+<?php
+
+echo "Luis Felipe Melo. <br><br>
+    Prueba de conectar o sincronizar <br>
+    en tiempo real lo que se ve en los <br>
+    archivos html y php activando <br>
+    live server en conjunto.<br><br>";
+
+for ($i=0; $i < 25; $i++)
+    {
+      //echo "$i";
+      echo "$i<br>";
+    }    
+?>
